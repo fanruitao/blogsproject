88d202132318e38539f981f4067284ad45d08a08
fans <xinyuan123.sheng@163.com>
2018-04-28 09:16:40 +0800

    Initial commit

---
+++ blogsproject/blog/views.py
@@ -0,0 +1,33 @@
+
+from django.shortcuts import render,get_object_or_404
+from django.http import  HttpResponse
+from .models import Post,Category
+import markdown
+
+def index(request):
+    post_list=Post.objects.all().order_by('-create_time')
+    return render(request,'blog/index.html',context={'title':'博客首页','post_list':post_list})
+def detail(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.increase_views()
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc',
+                                  ])
+    return render(request, 'blog/detail.html', context={'post': post})
+
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(create_time__year=year,
+                                    create_time__month=month
+                                    ).order_by('-create_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+def category(request, pk):
+
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-create_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
